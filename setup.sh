510ff51ccb919a9efe6ca38fa97864f611701de5
songmuhan <songmuhan99@gmail.com>
2024-02-02 16:50:38 +0800

    remove root environment
    Signed-off-by: songmuhan <songmuhan99@gmail.com>

--- setup.sh
+++ setup.sh
@@ -79,9 +79,6 @@ function test_all_env() {
 function setup_utils() {
    sudo apt-get update
    sudo apt-get --yes install neovim tmux htop autojump
-
-   sudo echo 'export PATH="/tip/miniforge3/bin:$PATH"'>> /root/.bashrc
-   sudo echo '. /usr/share/autojump/autojump.sh' >> /root/.bashrc
 }
 
 function setup_git_info() {
